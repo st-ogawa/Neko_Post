08c84785be1afbfae5d867f78846f36ff1319c9e
st-ogawa <kuromitu@outlook.jp>
2021-05-25 15:56:02 +0900

    タブの再利用見直し

--- resources/js/components/Pages/UserAuth.vue
+++ resources/js/components/Pages/UserAuth.vue
@@ -3,7 +3,9 @@
     <Header/>
     <div class="container">
       <div class="auth-card">
-        <LoginTab/>
+          <div class="swith-tab">
+            <LoginTab v-for="item in list" :key="item.id" v-bind="item" v-model="currentId"/>
+          </div>
         <router-view/>
       </div>
     </div>
@@ -17,6 +19,14 @@ import LoginTab from '../SheredParts/LoginTab.vue'
 import Header from '../Unit/Header'
 export default {
   components:{ Header, LoginTab, },
-  
+  data() {
+    return {
+      currentId:1,
+      list:[
+        {id: 1 , label: 'ログイン', link: 'login'},
+        {id: 2, label: '新規登録', link: 'register'}
+      ]
+    }
+  },
 }
 </script>
--- resources/js/components/SheredParts/LoginTab.vue
+++ resources/js/components/SheredParts/LoginTab.vue
@@ -1,41 +1,44 @@
 <template>
   <div class="LoginTab">
-      <nav>
-          <ul class="swith-tab">
-            <li :class="{leftTab:isActive}" @click.prevent="switchTab(1)" :disabled="loginActive">
-              <router-link to="login">ログイン</router-link>
-            </li>
-            <li :class="{rightTab:!isActive}" @click.prevent="switchTab(2)" :disabled="registerActive">
-              <router-link to="register">新規登録</router-link>
-            </li>
-          </ul>
-        </nav>
+    <button @click="switchTab" :class="[disabled, 'disabledTab']" :disabled="!disabled">
+      {{label}}   
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      loginActive : false,
-      registerActive : false,
-      isActive : false,
-    }
+  props:{
+    id: '',
+    label: '',
+    value: '',
+    link: String,
   },
   methods:{
-    switchTab(num){
-     
-      if(num === 1){
-        this.loginActive = true 
-        this.registerActive = false
-        this.isActive = false
-      } 
-      else if(num === 2){
-        this.loginActive = false 
-        this.registerActive = true
-        this.isActive = true
-      }
+    switchTab(){
+      console.log(link,value,id,label)
+      this.$emit('input', this.id)
+      this.$router.push(link)
     },
+    // switchTab(num){
+     
+    //   if(num === 1){
+    //     this.loginActive = true 
+    //     this.registerActive = false
+    //     this.isActive = false
+        
+    //   } 
+    //   else if(num === 2){
+    //     this.loginActive = false 
+    //     this.registerActive = true
+    //     this.isActive = true
+        
+    //   }
+      
+    // },
+    disabled(){
+      return this.value === this.id ? 'disabled' : false
+    }
     
   }
 }
