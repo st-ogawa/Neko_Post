e5669c06f3da17b21d8e97d07fad0e18e54de1b8
st-ogawa <kuromitu@outlook.jp>
2021-05-26 15:51:38 +0900

    tab修正

---
+++ resources/js/components/SheredParts/LinkTab.vue
@@ -0,0 +1,29 @@
+<template>
+  <li class="linkTab">
+      <div @click="switchTab" :class="[active, 'disabledTab']">
+        {{label}}   
+      </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props:{
+    id: Number,
+    label: String,
+    value: Number,
+    link: String,
+  },
+  computed:{
+    active(){
+      return this.value === this.id ? 'active' : false
+    }
+  },
+  methods:{
+    switchTab(){
+      this.$emit('input', this.id)
+      this.$router.push(this.link, () => {})
+    },    
+  }
+}
+</script>
