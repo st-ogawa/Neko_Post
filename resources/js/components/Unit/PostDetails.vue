607a6b4cdb4a23cae13166aff65a1a20323d817d
st-ogawa <kuromitu@outlook.jp>
2021-06-14 15:54:01 +0900

    クラス名変更

--- resources/js/components/Unit/PostDetails.vue
+++ resources/js/components/Unit/PostDetails.vue
@@ -10,17 +10,17 @@
               </div>
               <div class="detail">
                 <div class="user-status"></div>
-                <div class="post-detail-comment">
+                <div class="detail-comment">
                   {{item.comment}}
                 </div>
               </div>
             </div>
-            <div class="close-button" @click="$emit('close')">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="bevel">
-              <line x1="18" y1="1" x2="1" y2="18"/>
-              <line x1="1" y1="1" x2="18" y2="18"/>
-            </svg>
-          </div>
+            <div class="close-button" @click="$emit('close')" title="閉じる">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="bevel">
+                <line x1="18" y1="1" x2="1" y2="18"/>
+                <line x1="1" y1="1" x2="18" y2="18"/>
+              </svg>
+          　</div>
           </div>
         </div>
       </div>
