b6502630f8938881340d39978744cb6c0996fa8a
st-ogawa <kuromitu@outlook.jp>
2021-08-06 11:23:43 +0900

    console.log削除

--- resources/js/components/Pages/Discovery.vue
+++ resources/js/components/Pages/Discovery.vue
@@ -58,7 +58,6 @@ export default {
       if(this.load)return
       this.load = true
       this.loading = true
-      console.log('a')
       axios.get('/api/posts?page=' + this.page)
       .then(res=>{
         this.items = res.data.posts.slice(this.page, this.perPage)
