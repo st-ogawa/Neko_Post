de513d9e93774dfef5d6486c09cee4d5589c80a6
st-ogawa <kuromitu@outlook.jp>
2021-08-16 14:21:33 +0900

    画像パス変更

--- resources/js/components/Pages/Discovery.vue
+++ resources/js/components/Pages/Discovery.vue
@@ -61,7 +61,7 @@ export default {
       axios.get('https://neko-post.com/api/posts?page=' + this.page)
       .then(res=>{
         this.items = res.data.posts.slice(this.page, this.perPage)
-        console.log(this.items)
+        console.log(this.items);
         this.loading = false
         this.load = false
       }).catch(err=>{
