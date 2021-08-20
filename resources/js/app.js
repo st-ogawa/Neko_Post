ed2c49a0433c204cc4afc8255ad5026ce02a1183
st-ogawa <kuromitu@outlook.jp>
2021-08-20 10:40:49 +0900

    baseURL修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import router from './router'
 import store from './store/index'
 import './scroll'
-import axios from 'axios'
+
 require('./bootstrap');
 
 window.Vue = require('vue');
 
 Vue.component('app', require('./components/App').default);
 
-axios.defaults.baseURL = process.env.MIX_API_URL
+
 
 const app = new Vue({
     el: '#app',
